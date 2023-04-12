d32e084de24e97bc9f868d018255a28f601ab0fd
Arvydas Pečiulis <arvydas.peciulis23@gmail.com>
2023-04-12 21:00:25 +0300

    formData pushed to localStorage

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function backgroundBlur() {
-	const bodyEl = document.querySelectorAll(
-		"body > :not(.popUp), body > :not(.popUp) *"
-	);
+	const bodyEl = document.querySelectorAll("body > :not(.popUp):not(.popUp *)");
 	for (let i = 0; i < bodyEl.length; i++) {
 		bodyEl[i].style.filter = "blur(20px)";
 	}
@@ -18,4 +16,36 @@ function addBook() {
 	addBtn.addEventListener("click", popUpVisible);
 }
 
+function fetchFormData() {
+	const bookName = document.querySelector("#bookName").value;
+	const author = document.querySelector("#author").value;
+	const category = document.querySelector("#category").value;
+	const year = document.querySelector("#year").value;
+	const price = document.querySelector("#price").value;
+	const imgUrl = document.querySelector("#imgUrl").value;
+
+	return {
+		bookName,
+		author,
+		category,
+		year,
+		price,
+		imgUrl,
+	};
+}
+
+function pushData(formData) {
+	const bookName = formData.bookName;
+	localStorage.setItem(`${bookName}`, JSON.stringify(formData));
+}
+
+function formValidation() {
+	const form = document.querySelector(".addBookForm");
+	form.addEventListener("submit", (e) => {
+		e.preventDefault();
+		pushData(fetchFormData());
+	});
+}
+
 addBook();
+formValidation();
